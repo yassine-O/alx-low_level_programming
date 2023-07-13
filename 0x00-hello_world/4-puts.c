a012be66e29c0a05eb4abc52ea3854ee1f782d23
Yassine <yassine.ouaal@gmail.com>
2023-07-13 12:10:28 -0700

    Correct scripts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
-int main () {
-   puts("Programming is like building a multilingual puzzle");
-   
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void) 
+{
+   puts("Programming is like building a multilingual puzzle\n");
+
    return(0);
 }
