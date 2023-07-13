f8efc37d3d2306e64fab248fd1d7fb9271591ee3
Yassine <yassine.ouaal@gmail.com>
2023-07-13 12:35:10 -0700

    Advanced scripts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+  
+for (int i = 0; str[i] != '\0'; i++)
+	putchar(str[i]);
+  
+return (0);
+}
