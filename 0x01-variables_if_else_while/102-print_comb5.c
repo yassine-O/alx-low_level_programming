1b72ec7f2d39bca11e51f34b5cc3b1e935d9ea52
Yassine <yassine.ouaal@gmail.com>
2023-07-15 10:33:55 -0700

    Correct program

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -23,7 +23,7 @@ int main(void)
 					putchar(' ');
 					putchar('0' + k);
 					putchar('0' + l);
-					if (i != 9 && j != 8)
+					if (i != 9 || j != 8)
 					{
 						putchar(',');
 						putchar(' ');
