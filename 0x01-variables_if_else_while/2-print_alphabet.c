a0ebf23f618f6624144f27637789b31f1272212a
Yassine <yassine.ouaal@gmail.com>
2023-07-14 17:26:33 -0700

    Correct script

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	char chr;
 
-	for(chr='a'; chr <= 'z'; chr++)
+	for (chr = 'a'; chr <= 'z'; chr++)
 		putchar(chr);
 	putchar('\n');
 	return (0);
--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,10 +9,10 @@ int main(void)
 {
 	char chr;
 
-	for(chr='a'; chr <= 'z'; chr++)
+	for (chr = 'a'; chr <= 'z'; chr++)
 		putchar(chr);
 
-	for(chr='A'; chr <= 'Z'; chr++)
+	for (chr = 'A'; chr <= 'Z'; chr++)
 		putchar(chr);
 
 	putchar('\n');
