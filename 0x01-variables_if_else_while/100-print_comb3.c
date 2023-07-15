4d39766ad9d284d9f20e67fef1ce594effd4b52f
Yassine <yassine.ouaal@gmail.com>
2023-07-15 07:16:17 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,11 +7,11 @@
  */
 int main(void)
 {
-	int i,j;
+	int i, j;
 
 	for (i = 0; i <= 8; i++)
 	{
-		for (j = i+1; j <= 9; j++)
+		for (j = i + 1; j <= 9; j++)
 		{
 			putchar('0' + i);
 			putchar('0' + j);
