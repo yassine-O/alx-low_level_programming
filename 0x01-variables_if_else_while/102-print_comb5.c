c9ce6a34634f437efcac31ca6012cbfa423b8c90
Yassine <yassine.ouaal@gmail.com>
2023-07-15 07:34:41 -0700

    Advanced tasks

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = i + 1; j <= 8; j++)
+		{
+			for (k = j + 1; k <= 9; k++)
+			{
+				for (l = k + 1; l <= 9; l++)
+				{
+					putchar('0' + i);
+					putchar('0' + j);
+					putchar(' ');
+					putchar('0' + k);
+					putchar('0' + l);
+					if (i != 9 && j != 8)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
