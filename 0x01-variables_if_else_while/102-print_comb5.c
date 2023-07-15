80ae66147ddf85ac487de3dae22484cbe5c3fb7b
Yassine <yassine.ouaal@gmail.com>
2023-07-15 07:37:56 -0700

    Correct program

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,11 +11,11 @@ int main(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = i + 1; j <= 8; j++)
+		for (j = 0; j <= 8; j++)
 		{
-			for (k = j + 1; k <= 9; k++)
+			for (k = 0; k <= 9; k++)
 			{
-				for (l = k + 1; l <= 9; l++)
+				for (l = 1; l <= 9; l++)
 				{
 					putchar('0' + i);
 					putchar('0' + j);
