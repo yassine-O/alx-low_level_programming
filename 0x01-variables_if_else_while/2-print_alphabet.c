b550e0abd90ed58bb0462699274765e6c3a30c43
Yassine <yassine.ouaal@gmail.com>
2023-07-14 17:12:41 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char chr;
+    
+    for(chr='a'; chr <= 'z'; chr++)
+        putchar(chr);
+    putchar('\n');
+    return (0);
+}
