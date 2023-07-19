81105700999b6f4e21c933e790852d0d664cdf3f
Yassine <yassine.ouaal@gmail.com>
2023-07-19 11:51:31 -0700

    First commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "_putchar.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char str[] = "_putchar";
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+
+	return (0);
+}
