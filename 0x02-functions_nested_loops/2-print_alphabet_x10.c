a6c6d7f36a47a239bfaad1d93e2324cb6955bd5a
Yassine <yassine.ouaal@gmail.com>
2023-07-19 12:46:09 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Description
+ * Description: prints lower alphabets
+ * void
+ */
+void print_alphabet_x10(void)
+{
+	char chr;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (chr = 'a'; chr <= 'z'; chr++)
+			_putchar(chr);
+		_putchar('\n');
+	}
+}
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - Description
+ * @c : char input
+ * Description: prints lower alphabets
+ * Return: 1 or 0
+ */
+int _islower(int c)
+{
+	if ('a' <= c <= 'z')
+		return (1)
+	else
+		return (0);
+}
