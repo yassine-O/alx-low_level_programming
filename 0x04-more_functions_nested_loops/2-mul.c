5a40b7ead0718945d3d0bb20c8a78d24f7b9ae68
Yassine <yassine.ouaal@gmail.com>
2023-07-21 14:21:54 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Description
+ * @a : char input
+ * @b : input
+ * Description: prints lower alphabets
+ * Return: int
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
