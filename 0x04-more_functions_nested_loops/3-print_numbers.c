91873d1b0e9862b0ec5385c1df6c9c6ea72f5dd4
Yassine <yassine.ouaal@gmail.com>
2023-07-21 14:27:16 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - Description
+ * Description: prints lower alphabets
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		_putchar('0' + i);
+
+	_putchar('\n');
+}
