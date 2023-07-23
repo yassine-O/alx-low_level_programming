e1faeacedc2d4fdb6d41f1e1a3a84012a2edd60a
Yassine <yassine.ouaal@gmail.com>
2023-07-23 11:11:41 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Description
+ * @a : int input
+ * @b : int input
+ * Description: prints lower alphabets
+ * Return: 1 or 0
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
