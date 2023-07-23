f7f8cea24647c70c6d34f53c5c1d24748f297c56
Yassine <yassine.ouaal@gmail.com>
2023-07-23 11:19:12 -0700

    int i;

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - Description
+ * @n: int input
+ * Description: prints lower alphabets
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i <= 98; i++)
+	{
+		printf("%d", i);
+		if (i != 98)
+			printf(", ");
+	}
+}
