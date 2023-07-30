ab5e1e53a4d615f59840c956b8e145b6b78978db
Yassine <yassine.ouaal@gmail.com>
2023-07-30 08:52:22 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcpy - Description
+ * @dest: pointer
+ * @src: pointer
+ * Description: pointers
+ * Return: dest pointer
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i, n = strlen(src);
+
+	for (i = 0; i <= n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
