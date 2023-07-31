cd290050678973d70c2abc8aa44d4c67f6ca1279
Yassine <yassine.ouaal@gmail.com>
2023-07-31 07:52:53 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memset - Description
+ * @s: pointer
+ * @b: char
+ * @n: int
+ * Description: pointers
+ * Return: pointer
+ */
+char *_memset(char *s, char b, unsigned int n);
+{
+	char *p = s;
+
+	while (n--)
+	{
+		*s = b;
+		s++;
+	}
+
+	return (p);
+}
