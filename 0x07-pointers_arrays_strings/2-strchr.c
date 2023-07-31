dc80e310af11516ede4b4b18dae222dbb79d96c6
Yassine <yassine.ouaal@gmail.com>
2023-07-31 08:46:33 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strchr - Description
+ * @s: pointer
+ * @c: char
+ * Description: pointers
+ * Return: pointer
+ */
+char *_strchr(char *s, char c)
+{
+	char *p = NULL;
+
+	while (*s)
+	{
+		if (*s == c)
+		{
+			p = s;
+			break;
+		}
+		s++;
+	}
+
+	return (p);
+}
