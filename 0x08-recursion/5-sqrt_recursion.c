55abb40f3ded170cfb0da1380c69dde3d1689c65
Yassine <yassine.ouaal@gmail.com>
2023-08-02 12:12:43 -0700

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - Description
+ * @n: int
+ * Description: recursion
+ * Return: void
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	return (_sqrt(1, n));
+}
+
+/**
+ * _sqrt - Description
+ * @k: int
+ * @n: int
+ * Return: int
+ */
+int _sqrt(int k, int n)
+{
+	if (k > n)
+		return (-1);
+
+	if (k * k == n)
+		return (k);
+
+	return (_sqrt(k + 1, n));
+}
