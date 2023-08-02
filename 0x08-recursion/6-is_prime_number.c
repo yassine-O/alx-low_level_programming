984ee77bff737dc875d3114a97c4458c206c7045
Yassine <yassine.ouaal@gmail.com>
2023-08-02 15:09:37 -0700

    6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * is_prime_number - Description
+ * @n: int
+ * Description: recursion
+ * Return: int
+ */
+int is_prime_number(int n)
+{
+	if (n < 2)
+		return (0);
+
+	return (_prime(2, n));
+}
+
+/**
+ * _prime - Description
+ * @k: int
+ * @n: int
+ * Return: int
+ */
+int _prime(int k, int n)
+{
+	if (k == n)
+		return (1);
+
+	if (k > n || n % k == 0)
+		return (0);
+
+	return (_prime(k + 1, n));
+}
