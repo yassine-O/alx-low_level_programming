d891087607492059060506409cfd214e228c91b3
Yassine <yassine.ouaal@gmail.com>
2023-08-03 11:41:47 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-int _strlen(char *s);
-
 /**
  * wildcmp - Description
  * @s1: pointer
@@ -11,46 +9,18 @@ int _strlen(char *s);
  */
 int wildcmp(char *s1, char *s2)
 {
-	if(*s2 == '*')
-		return (wildcmp(s1, s2 + 1));
-
-	if (*s1 != *s2)
-		return (0);
-
-	s2 = _next_char(s2);
-
-	if(!*s2)
+	if (!*s1 && !*s2)
 		return (1);
 
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
 
-}
+	if (*s2 == '*')
+	{
+		if (!*(s2 + 1))
+			return (1);
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	}
 
-/**
- * _strlen - Description
- * @s: pointer
- * Return: int
- */
-char *_next_char(char *s)
-{
-	if (*s == '*')
-		return (_next_char(s + 1));
-
-	return (s);
-}
-
-/**
- *  - Description
- * @s: pointer
- * @n: int
- * Return: int
- */
-char * _find_char(char *s, char ch)
-{
-	if (!*s)
-		return (NULL);
-
-	if (*s == ch)
-		return s;
-
-	return _find_char(s + 1, ch);
+	return (0);
 }
