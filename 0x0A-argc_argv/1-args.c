4e8ef904d07013dab266cfb875a1c555955d74a3
Yassine <yassine.ouaal@gmail.com>
2023-08-06 07:15:02 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: int
+ * @argv: pointers
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
