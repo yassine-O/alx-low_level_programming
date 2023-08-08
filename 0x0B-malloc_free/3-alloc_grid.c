47c127f3e9427bdbf8e2c0f71a4a945ad62f95a2
Yassine <yassine.ouaal@gmail.com>
2023-08-08 14:54:22 -0700

    3-alloc_grid.c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - Description
+ * @width: int
+ * @height: int
+ * Description: recursion
+ * Return: pointer
+ */
+int **alloc_grid(int width, int height)
+{
+	int **arr;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	arr = malloc(height * sizeof(int *));
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		arr[i] = malloc(width * sizeof(int));
+		if (arr[i] == NULL)
+			return (NULL);
+	}
+
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			arr[i][j] = 0;
+
+	return (arr);
+}
