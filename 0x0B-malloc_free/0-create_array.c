c04f614f3956c52471d3102b265d98f0bea72e80
Yassine <yassine.ouaal@gmail.com>
2023-08-08 12:09:51 -0700

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - Description
+ * @size: int
+ * @c: char
+ * Description: recursion
+ * Return: pointer
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+
+	if (size == 0)
+		return (NULL);
+
+	arr = malloc(size);
+
+	while (--size)
+		*(arr + size) = c;
+
+	return (arr);
+}
