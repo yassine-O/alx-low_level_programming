58118dd063e3f43432877b39e0df7d7cd63ed968
Yassine <yassine.ouaal@gmail.com>
2023-08-09 13:24:44 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -13,7 +13,7 @@ char **strtow(char *str)
 	char **words;
 	int n = 0, m = 0, i, j, k;
 
-	if (str == NULL || str == "")
+	if (str == NULL || *str == '\0')
 		return (NULL);
 
 	for (i = 0; str[i]; i++)
