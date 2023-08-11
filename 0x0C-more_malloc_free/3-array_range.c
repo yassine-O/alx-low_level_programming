9b28403315653ec4dec5cacf565e644c58a8bb64
Yassine <yassine.ouaal@gmail.com>
2023-08-11 11:05:25 -0700

    3-array_range.c

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * array_range - Description
+ * @min: int
+ * @max: int
+ * Description: recursion
+ * Return: pointer
+ */
+int *array_range(int min, int max)
+{
+	int i;
+	int *arr;
+
+	if (min > max)
+		return (NULL);
+
+	arr = malloc(max - min + 1);
+
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < max - min + 1; i++)
+		arr[i] = min + i;
+
+	return (arr);
+}
