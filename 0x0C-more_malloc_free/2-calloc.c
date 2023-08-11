e5bea8e8adf96c7299ef28d3fc92ef9d7c4a7e1d
Yassine <yassine.ouaal@gmail.com>
2023-08-11 10:43:33 -0700

    2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _calloc - Description
+ * @nmemb: int
+ * @size: int
+ * Description: recursion
+ * Return: pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *p;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	p = malloc(nmemb * size);
+
+	return (NULL);
+}
