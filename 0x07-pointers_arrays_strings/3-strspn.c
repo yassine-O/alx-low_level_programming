78d262c97cc394cba5099c6a826cd614a53a796a
Yassine <yassine.ouaal@gmail.com>
2023-08-12 11:44:05 -0700

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strspn - Description
+ * @s: pointer
+ * @accept: pointer
+ * Description: pointers
+ * Return: unsigned int
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int n = 0;
+	char *ch;
+
+	while (*s)
+	{
+		ch = accept;
+		while (*ch)
+		{
+			if (*s == *ch)
+			{
+				n++;
+				break;
+			}
+			ch++;
+		}
+
+		if (*ch == '\0')
+			break;
+
+		s++;
+	}
+
+	return (n);
+}
