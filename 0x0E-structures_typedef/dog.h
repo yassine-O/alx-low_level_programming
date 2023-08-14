d57ff265da47eb337e8545c5d09dbafc840c8028
Yassine <yassine.ouaal@gmail.com>
2023-08-14 12:15:37 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
