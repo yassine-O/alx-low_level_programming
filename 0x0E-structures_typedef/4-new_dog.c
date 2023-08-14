594e3ba26b0d2fd1a0afc8ae2c1c78f640b892b2
Yassine <yassine.ouaal@gmail.com>
2023-08-14 16:20:15 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,15 +13,36 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
+	char *dname, *downer;
+	int i, len1 = 0, len2 = 0;
 
 	d = malloc(sizeof(dog_t));
 
 	if (d == NULL)
 		return (NULL);
 
-	d->name = name;
+	while (name[len1])
+		len1++;
+	while (owner[len2])
+		len2++;
+
+	dname = malloc(len1 + 1);
+	downer = malloc(len2 + 1);
+
+	if (dname == NULL || downer == NULL)
+		return (NULL);
+
+	for (i = 0; i < len1; i++)
+		dname[i] = name[i];
+	dname[i] = '\0';
+
+	for (i = 0; i < len2; i++)
+		downer[i] = owner[i];
+	downer[i] = '\0';
+
+	d->name = dname;
 	d->age = age;
-	d->owner = owner;
+	d->owner = downer;
 
 	return (d);
 }
