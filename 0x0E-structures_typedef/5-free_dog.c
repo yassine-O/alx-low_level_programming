bf1bbba1f8ce4d4b667f692447747cf41098a6c3
Yassine <yassine.ouaal@gmail.com>
2023-08-15 13:14:30 -0700

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - Description
+ * @d: struct
+ * Description: pointers
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
