f6515e303ec104c45344a57f2237202102700ae9
Yassine <yassine.ouaal@gmail.com>
2023-08-28 14:57:39 -0700

    6-pop_listint.c

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * pop_listint - Description
+ * @head: struct
+ * Description: linked list
+ * Return: int
+ */
+int pop_listint(listint_t **head)
+{
+	int n;
+	listint_t *tmp;
+
+	if (head == NULL)
+		return (0);
+
+	tmp = *head;
+	n = tmp->n;
+	*head = (*head)->next;
+	free(tmp);
+
+	return (n);
+}
