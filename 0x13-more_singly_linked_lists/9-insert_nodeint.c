e25dfd23584d4778bc1ad9edba19dbdef90ee47f
Yassine <yassine.ouaal@gmail.com>
2023-08-29 14:20:16 -0700

    9-insert_nodeint.c

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - Description
+ * @head: struct
+ * @idx: int
+ * @n: int
+ * Description: linked list
+ * Return: struct
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i = 0;
+	listint_t *tmp, *node = NULL;
+
+	node = malloc(sizeof(listint_t));
+	if (node == NULL)
+		return (NULL);
+	node->n = n;
+
+	if (idx == 0)
+	{
+		node->next = (*head)->next;
+		*head = node;
+		return (node);
+	}
+
+	tmp = *head;
+	while (tmp != NULL)
+	{
+		if (i == idx - 1)
+		{
+			node->next = tmp->next;
+			tmp->next = node;
+			break;
+		}
+		i++;
+		tmp = tmp->next;
+	}
+
+	return (node);
+}
