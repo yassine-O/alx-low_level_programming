c479fe1b336f26c06c1d450a8ac082b60b46fa5c
Yassine <yassine.ouaal@gmail.com>
2023-09-04 14:53:55 -0700

    0-read_textfile.c

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,12 @@
+#ifndef main_h
+#define main_h
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
