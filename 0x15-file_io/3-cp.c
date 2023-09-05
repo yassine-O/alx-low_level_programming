e79c59d0dfd577c0b07a4d0419aefe9d0ea26fea
Yassine <yassine.ouaal@gmail.com>
2023-09-05 15:05:58 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -75,7 +75,9 @@ int main(int argc, char *argv[])
  */
 void _close(int fd)
 {
-	if (!close(fd))
+	int err = close(fd);
+
+	if (err < 0)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 	exit(100);
 }
