a60756a865f041784100264c8af6737636adc07e
Yassine <yassine.ouaal@gmail.com>
2023-09-22 14:29:51 -0700

    3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,7 +9,14 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	*n |= 1 << index;
+	unsigned long int k = 1;
+
+	k = k << index;
+
+	if (k == 0)
+		return (-1);
+
+	*n |= k;
 
 	return (1);
 }
