22c8104e42bd55e33b6c69b34d7e806381eb741f
Yassine <ouaal.yassine@gmail.com>
2023-10-26 23:33:09 +0100

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,5 +9,11 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
+	unsigned int max_bits;
+
+	max_bits = (sizeof(unsigned long int) * 8);
+	if (index > max_bits)
+		return (-1);
+
 	return ((n >> index) & 1);
 }
