495f3d9d5e42e591727f2eb1fa68455049364250
Yassine <yassine@gmail.com>
2024-04-11 16:29:49 -0700

    9. Interpolation search

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,32 @@
+#include "search_algos.h"
+
+/**
+ * interpolation_search - Description
+ * @array: pointer
+ * @size: int
+ * @value: int
+ * Description: search algorithm
+ * Return: int
+ */
+int interpolation_search(int *array, size_t size, int value)
+{
+	size_t pos, low = 0, high = size - 1;
+
+	if (array == NULL)
+		return (-1);
+
+	pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
+
+	while (pos < size)
+	{
+		printf("Value checked array[%ld] = [%d]\n", pos, array[pos]);
+
+		if (array[pos] == value)
+			return (pos);
+		pos++;
+	}
+
+	printf("Value checked array[%ld] is out of range\n", pos);
+
+	return (-1);
+}
